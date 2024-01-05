5aa644a02a2a177d44785cd2f40f136bdf99d031
Mikk0git <kasprzakmilan1@gmail.com>
2024-01-05 18:31:08 +0100

    Basic article scraping (bbc)

--- Program.cs
+++ Program.cs
@@ -1,47 +1,90 @@
-﻿namespace newsVideoGenerator
+﻿using HtmlAgilityPack;
+
+namespace newsVideoGenerator
 {
+    public class Article
+    {
+        public string? url { get; set; }
+        public string? title { get; set; }
+        public string? content { get; set; }
+    }
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("---NEWS VIDEO GEN---");
 
             Random random = new Random();
             int id = random.Next(10000, 100000);
 
-
-            List <Article> articleList = new List<Article>();
+            List<Article> articleList = new List<Article>();
             foreach (var url in args)
             {
-                articleList.Add(scrapeArticle(url));
+                // Await the asynchronous method call
+                Article article = await scrapeArticleAsync(url);
+                articleList.Add(article);
             }
 
             string script = GenerateScript(articleList);
 
-            GenerateAudio(script,id);
+            GenerateAudio(script, id);
 
             MakeVideo(id);
-
         }
 
-        public class Article
-        {
-            public string? url { get; set; }
-            public string? title { get; set; }
-            public string? content { get; set; }
-        }
-        static Article scrapeArticle(string url)
+
+        static async Task<Article> scrapeArticleAsync(string url)
         {
             Article article = new Article();
             Console.WriteLine($"Scraping {url}");
 
 
             article.url = url;
-            article.title = "dasklda";
-            article.content = "dasdkamkdmaksdmkamdkmaskdmaksmdkamsdkmams";
+
+
+            HttpClient client = new HttpClient();
+            string html = await client.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            Console.WriteLine($"{url} source scraped");
+
+            switch (true)
+            {
+                case var _ when url.Contains("bbc"):
+                    Console.WriteLine("BBC article found");
+                    article = mineBBC(article, htmlDocument);
+                    break;
+                default:
+                    Console.WriteLine("Source unknown");
+                    break;
+            }
+
+
 
             return article;
         }
+
+        static Article mineBBC(Article article, HtmlDocument htmlDocument)
+        {
+            article.title = htmlDocument.DocumentNode.SelectSingleNode("//title").InnerText;
+            //Console.WriteLine($"Title: {article.title}");
+
+            var contentNodes = htmlDocument.DocumentNode.SelectNodes("//p[@class='ssrcss-1q0x1qg-Paragraph e1jhz7w10']");
+            if (contentNodes != null)
+            {
+                article.content = string.Join("\n", contentNodes.Select(node => node.InnerText));
+                //Console.WriteLine($"Content: {article.content}");
+            }
+            else
+            {
+                Console.WriteLine("Content paragraphs not found.");
+            }
+
+
+
+            return article;
+        }
+
 
         static string GenerateScript(List <Article> articleList)
         {
